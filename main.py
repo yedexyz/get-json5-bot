887db65484b1f39e1656e3055e27eefb2d61493a
Dmitry Porunov <dmitr.porunov@gmail.com>
2020-12-09 21:35:16 +0300

    Sometimes I believe compiler ignores all my comments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from urllib.parse import urljoin
+
 import json5
 import logging
 import os
@@ -5,8 +7,16 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ContentTypes
 from aiogram.utils import executor
+from aiogram.utils.executor import start_webhook
 
-API_TOKEN: str = os.environ["API_TOKEN"]  # 1231231231:SALAD12_SAddamsASASJDJAKSDJKSADASs
+API_TOKEN: str = os.environ["API_TOKEN"]  # 1231231231:SALAD12_SAddamsASASJDJAKSDJKSADASf
+PROJECT_NAME: str = os.environ["PROJECT_NAME"]  # Set it as you've set TOKEN env var
+HOST = "0.0.0.0"
+PORT: str = os.environ["PORT"]
+
+WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com/'  # Enter here your link from Heroku project settings
+WEBHOOK_URL_PATH = '/webhook/' + API_TOKEN
+WEBHOOK_URL = urljoin(WEBHOOK_HOST, WEBHOOK_URL_PATH)
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
@@ -19,5 +29,16 @@ async def json_please(message: types.Message):
                          parse_mode="html")
 
 
+async def on_startup(_):
+    await bot.set_webhook(WEBHOOK_URL)
+    logging.info(dp)
+
+
+async def on_shutdown(_):
+    logging.info(dp)
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_URL_PATH,
+                  on_startup=on_startup, on_shutdown=on_shutdown,
+                  host=HOST, port=PORT)
